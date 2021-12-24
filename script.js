ef07e2a0f5e19eba0b1e8cc060c284a837a678f3
joaoalvesmuniz <joaovitoralvesmuniz98@gmail.com>
2021-12-24 13:25:21 -0300

    seila

---
+++ script.js
@@ -0,0 +1,237 @@
+
+let cartaIsabel = {
+
+    nome:"Isabel Aráujo dos Santos",
+    imagem:"https://www.superprof.com.br/imagens/anuncios/professor-home-estudante-ingles-avancado-aulas-conversacao-gramatica-para-quem-interessa-pela-lingua.jpg",
+    atributos: {
+        Beleza: 100,
+        Habilidades: 90,
+        Sorte: 0
+    }
+   
+}
+let cartaJoao = {
+
+    nome:"João Vitor Alves Muniz",
+    imagem:"https://www.tiktok.com/api/img/?itemId=6884610300478360834&location=1&aid=1988",
+    atributos :{
+        Beleza: 99,
+        Habilidades: 10,
+        Sorte: 100,
+    }
+}
+
+let cartaValeria = {
+    nome:"Valeria Girl",
+    imagem: "https://scontent.fcgh51-1.fna.fbcdn.net/v/t1.6435-9/139485351_2731272503804653_4752067475996243748_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFR2VR210ASuSLY5P_KSzZ1bm5c0_BloKtublzT8GWgq_WNuJFHmiw5x0Qu46gqVMI_ebYN4kwMUf1RZQWgyvgT&_nc_ohc=b1cbTx5E8oAAX9tAODM&_nc_pt=1&_nc_ht=scontent.fcgh51-1.fna&oh=00_AT-eR9Ux73m_Okl82Kd5n3SQ_Lhk9XNHZ7dZIy8AFziP4w&oe=61DD0A3C",
+    atributos :{
+        Beleza: 89,
+        Habilidades: 100,
+        Sorte: 10,
+    }
+}
+let cartaIsabel2015 = {
+    nome:"Isabel 2015",
+    imagem: "https://scontent.fcgh51-1.fna.fbcdn.net/v/t1.18169-9/10426246_1417727825222966_3157936490648295214_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=174925&_nc_eui2=AeG35Bc0CXWDE7uW5Z9ppKgKgGnokOegLjCAaeiQ56AuMP10v-8hzVKavwkCe3dDiyeUqH8pO6jBMozynCgU-JM2&_nc_ohc=uJ-jcfG1sfwAX_2fFYH&_nc_pt=1&_nc_ht=scontent.fcgh51-1.fna&oh=00_AT8kW8EAVvX8b645R_Sv6iY8GlF1ngQidxzOaGOrJJnZow&oe=61ECBF78",
+    atributos :{
+        Beleza: 10,
+        Habilidades: 0,
+        Sorte: 50,
+    }
+
+}
+let cartaIsabel2016 = {
+    nome:"Isabel 2016",
+    imagem: "https://scontent.fcgh51-1.fna.fbcdn.net/v/t1.18169-9/10410226_1522181488110932_8978353715375320620_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=174925&_nc_eui2=AeHnz7dDwfSw7mIzvMbMLssGPeFIMhMN19E94UgyEw3X0TtAjowvUQFJJcK7dWfJ-wQLWUsQE8PUD7mPrWbSDKM7&_nc_ohc=AjWVmCjysDIAX9LHIi2&tn=ckOsTmoFxg8mBGlk&_nc_pt=1&_nc_ht=scontent.fcgh51-1.fna&oh=00_AT_fio3BvRNJ3h3h3ZCXwSCVy7V6wLL3wBCmxyjNg0qWpQ&oe=61ECD29D",
+    atributos :{
+        Beleza: 20,
+        Habilidades: 5,
+        Sorte: 60,
+    }
+
+}
+let cartaIsabel2018 = {
+    nome:"Isabel 2018",
+    imagem: "https://scontent.fcgh51-1.fna.fbcdn.net/v/t1.6435-9/37386441_2042355319426877_4476594376251801600_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_eui2=AeGQWFkn_eam5pXZZLRVd8nFe1Iz-sFIRc17UjP6wUhFzXbEDtbaUZ9U5Gefntj93zjESDQDR66YlbGpijqi6zHn&_nc_ohc=4qv4sqE2jXYAX-gFOQq&_nc_pt=1&_nc_ht=scontent.fcgh51-1.fna&oh=00_AT8Leksrp-xU7A87HGeRPDao84WlTbPobJLxpjOXvI_kiQ&oe=61EBA023",
+    atributos :{
+        Beleza: 50,
+        Habilidades: 15,
+        Sorte: 70,
+    }
+
+}
+let cartaTey = {
+    nome:"Tey Eterna",
+    imagem: "https://scontent.fcgh3-1.fna.fbcdn.net/v/t31.18172-8/19238190_1487544957934028_6098228539660296059_o.jpg?_nc_cat=109&ccb=1-5&_nc_sid=a9b1d2&_nc_eui2=AeHyi1s2LWQyRCJ8h_PkhIIOfh0wG3stXEN-HTAbey1cQ4XpffOqd8OdyDjB1jvhKCrrd9LHh7mIeUt8rEwyklzc&_nc_ohc=YkXSJMyrA54AX_VYMOn&tn=ckOsTmoFxg8mBGlk&_nc_pt=1&_nc_ht=scontent.fcgh3-1.fna&oh=00_AT9siaGrqynsuVmIaUZz5WuhWG8Qel827omMMgWZz6JL-A&oe=61EABF42",
+    atributos :{
+        Beleza: 1000000000000000,
+        Habilidades: 100000000000,
+        Sorte: 100000000000000000000,
+    }
+
+}
+let cartaAndre = {
+    nome:"Andre Carimpeiro",
+    imagem: "https://scontent.fcgh3-1.fna.fbcdn.net/v/t31.18172-8/19702579_1507683582586832_650160336797001690_o.jpg?_nc_cat=108&ccb=1-5&_nc_sid=a9b1d2&_nc_eui2=AeErMqYsjObEeH_Todzh29uFp3Tbmqd4vhyndNuap3i-HIt8AC4qzJM_Lx-NoXBWpHmT-zFY7leSbqBmoL3BdUVG&_nc_ohc=GDFtdN76lHIAX8dnZAt&tn=ckOsTmoFxg8mBGlk&_nc_pt=1&_nc_ht=scontent.fcgh3-1.fna&oh=00_AT-t-lg9mTMi1dx8RyHss_Rz9bEix1GYSGxKP_J6mxcVRw&oe=61E99377",
+    atributos :{
+        Beleza: 99,
+        Habilidades: 60,
+        Sorte: 10,
+    }
+
+}
+let cartaThor = {
+    nome:"Thor Fofis",
+    imagem: "https://scontent.fcgh3-1.fna.fbcdn.net/v/t1.18169-9/12033080_504447903047742_6696008339806665653_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=ba80b0&_nc_eui2=AeE9LASNqjpoqEgW0kVgmev0eD1HWptsuJJ4PUdam2y4kqT8JeFWEgizmPBvUSRN0abr0_Es155FhY6Iep_DGRye&_nc_ohc=yofrE0J0ltMAX81pFQn&_nc_pt=1&_nc_ht=scontent.fcgh3-1.fna&oh=00_AT-D0iwPuNWxYi172vunaWqI83PN94m2b8h3pR1kBUWriw&oe=61ED2B59",
+    atributos :{
+        Beleza: 1000,
+        Habilidades: 1000,
+        Sorte: 1000,
+    }
+
+}
+
+let cartaJoao2011 = {
+    nome:"Joao 2011",
+    imagem: "https://scontent.fcgh3-1.fna.fbcdn.net/v/t31.18172-8/1957780_277219425770592_243669115_o.jpg?_nc_cat=107&ccb=1-5&_nc_sid=2c4854&_nc_eui2=AeGQBOzlO5yHwDIFsHihPmCjMY99f2FC-QYxj31_YUL5Bsz1FFWFRStUa5c7SXC6LPsqMvXITQ5IqpUqCvD9ADS0&_nc_ohc=mNhFYIxLQGkAX_4wOy2&tn=ckOsTmoFxg8mBGlk&_nc_pt=1&_nc_ht=scontent.fcgh3-1.fna&oh=00_AT_Ul1LgL1w-ondcB01iwBXP7f8XiVUEgmm8tlCwl_ulbg&oe=61EC30DB",
+    atributos :{
+        Beleza: 50,
+        Habilidades: 0,
+        Sorte: 100,
+    }
+
+}
+
+
+
+
+
+let imagemMaquina = "https://i.pinimg.com/originals/71/1e/da/711eda25308c65a7756751088866e181.jpg"
+
+let arrayCartas = [cartaIsabel,cartaJoao,cartaValeria,cartaIsabel2015,cartaIsabel2016,cartaIsabel2018,cartaTey,cartaAndre,cartaThor,cartaJoao2011]
+let numeroCartas = arrayCartas.length
+
+
+let cartaJogador
+let cartaMaquina
+
+let btnDuelar = document.getElementById("btnDuelar");
+let btnSortear = document.getElementById("btnSortear")
+
+
+//Funcao que Sortea as Cartas
+btnSortear.addEventListener("click",function(){
+    
+    let div2 =  document.getElementById("conteudoMq");
+    div2.style.backgroundImage=`url(${imagemMaquina})`
+   let numeroCartaMaquina = parseInt(Math.random() * numeroCartas);
+   console.log(numeroCartaMaquina)
+   
+   cartaMaquina = arrayCartas[numeroCartaMaquina]
+   console.log(cartaMaquina)
+
+   let numeroCartaJogador = parseInt(Math.random() * numeroCartas);
+   while(numeroCartaJogador == numeroCartaMaquina){
+       numeroCartaJogador = parseInt(Math.random() * numeroCartas);
+   }
+   cartaJogador = arrayCartas[numeroCartaJogador];
+   console.log(cartaJogador)
+
+   btnDuelar.disabled = false;
+   exibeCartaJogador();
+   exibirOpcoes()
+   let divResultado = document.getElementById("winlose");
+   htmlResultado = "<p class='resultado-final'></p>";
+   divResultado.innerHTML = htmlResultado;
+
+
+   let divCartaMaquina = document.getElementById("conteudoMq")
+   divCartaMaquina.innerHTML = ""
+
+
+});
+
+function exibeCartaJogador(){
+
+        let divCartaJogador = document.getElementById("carta-jogador")
+        divCartaJogador.style.backgroundImage=`url(${cartaJogador.imagem})`
+        let nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+        divCartaJogador.innerHTML = nome
+}
+
+
+function exibirOpcoes(){
+
+     let opcoes = document.getElementById("opcoes")
+     let opcoesTexto = ""
+     for(let atributo in cartaJogador.atributos){
+         opcoesTexto += "<input  class='inputs' type='radio' name='atributo' value='" +atributo+"'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+
+
+         opcoes.innerHTML = opcoesTexto
+    }
+}
+
+function obterAtributoSelecionado(){
+     
+    let radioAtributo = document.getElementsByName('atributo')
+    for(let i = 0; i < radioAtributo.length;i++){
+
+        if(radioAtributo[i].checked){
+            console.log(radioAtributo[i].value)
+            return radioAtributo[i].value
+        }
+    }
+}
+
+function jogar(){
+
+   let obterAtributo = obterAtributoSelecionado();
+   let divResultado = document.getElementById("winlose");
+   
+   if(cartaJogador.atributos[obterAtributo] > cartaMaquina.atributos[obterAtributo]){
+   
+    htmlResultado = "<p class='resultado-final'>Venceu</p>";
+
+
+   }else if(cartaJogador.atributos[obterAtributo] < cartaMaquina.atributos[obterAtributo]){
+       
+       
+
+       htmlResultado = "<p class='resultado-final'>Perdeu</p>";
+
+   }else{
+
+        htmlResultado = "<p class='resultado-final'>Empatou</p>";
+   }
+
+   divResultado.innerHTML = htmlResultado;
+   exibeCartaMaquina();
+}
+
+function exibeCartaMaquina(){
+
+    let divCartaMaquina = document.getElementById("conteudoMq")
+    divCartaMaquina.style.backgroundImage=`url(${cartaMaquina.imagem})`
+    let nome = `<p class="carta-subtitle2">${cartaMaquina.nome}</p>`
+
+  
+    var tagHTML = "<div id='opcoes' class='opcoes12' class='carta-status'>";
+
+
+    var opcoesTexto = "";
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p  class='pzinho' type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  
+
+  divCartaMaquina.innerHTML = tagHTML + opcoesTexto + nome + "</div>";
+
+}
